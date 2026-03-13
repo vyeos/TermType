c1f94889a04e1fd0d2367ddd7070bdaf5ec40948
vyeos <rudranpatel0980@gmail.com>
2026-03-13 23:33:07 +0530

    add: shuffle words

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+func shuffleWords() {
+	dat, err := os.ReadFile("500_common_words.txt")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+
+	words := strings.Split(strings.TrimSpace(string(dat)), "\n")
+	rand.Shuffle(len(words), func(i, j int) {
+		words[i], words[j] = words[j], words[i]
+	})
+
+	for _, word := range words {
+		fmt.Println(word)
+	}
+}
 
 func main() {
-	fmt.Printf("Hello")
+	shuffleWords()
 }
